5b6c9c14056b91bc336cc481ebe076402e99191a
Jonas Huberts <66276492+jonashuberts@users.noreply.github.com>
2022-01-07 16:26:03 +0100

    x

--- test.py
+++ test.py
@@ -1,2 +1,5 @@
-import subprocess
-subprocess.call('sudo python3 /home/pi/badge/led-badge-11x44.py "Hello World!"', shell=False)
+#import subprocess
+#subprocess.call('sudo python3 /home/pi/badge/led-badge-11x44.py "Hello World!"', shell=False)
+
+import os
+os.system('sudo python3 /home/pi/badge/led-badge-11x44.py "Hello World!"')
