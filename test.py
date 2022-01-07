07923337470536987dd90034f33176480b252893
Jonas Huberts <66276492+jonashuberts@users.noreply.github.com>
2022-01-07 16:27:10 +0100

    x

--- test.py
+++ test.py
@@ -1,5 +1,8 @@
 #import subprocess
 #subprocess.call('sudo python3 /home/pi/badge/led-badge-11x44.py "Hello World!"', shell=False)
-
+import time
 import os
+
 os.system('sudo python3 /home/pi/badge/led-badge-11x44.py "Hello World!"')
+time.sleep(4)
+os.system('sudo python3 /home/pi/badge/led-badge-11x44.py "2"')
