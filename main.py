3851eb06ea6c9aa9756a03dc9e25561d77cd903c
Jonas Huberts <66276492+jonashuberts@users.noreply.github.com>
2022-01-08 12:44:33 +0100

    x

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import time
 import os
 from datetime import datetime as dt
+
 minute = dt.now().minute
+
 while True:
     if minute < dt.now().minute:
         os.system('sudo python3 /home/pi/badge/led-badge-11x44.py "%s"' % minute)
+        minute = dt.now().minute 
         print("update")
     else:
-       minute = dt.now().minute 
        print(".... wait ...")
-       time.sleep(1)
+       time.sleep(10)
