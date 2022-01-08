e2b39f36cf98230a785e7ef1f2dd2b5c7fd733a7
Jonas Huberts <66276492+jonashuberts@users.noreply.github.com>
2022-01-08 12:39:47 +0100

    x

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ from datetime import datetime as dt
 minute = dt.now().minute
 while true:
     if minute > dt.now().minute:
-        os.system(f'sudo python3 /home/pi/badge/led-badge-11x44.py "{minute = dt.now().minute}"')
+        os.system('sudo python3 /home/pi/badge/led-badge-11x44.py "%s"' % minute)
     else:
        minute = dt.now().minute 
